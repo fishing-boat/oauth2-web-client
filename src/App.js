893b232ab90b4f5246cb2899fe3cedb636ecbe2e
hbjs97 <hbjs97@naver.com>
2023-11-27 01:32:22 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useSearchParams } from 'react-router-dom';
+
+const App = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const authenticate = async () => {
+    const code = searchParams.get('code');
+    axios
+      .post('https://4ce2eof4f3.execute-api.ap-northeast-2.amazonaws.com/authenticate', { code })
+      .then((response) => {
+        console.log('Server response:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error sending code to the server:', error);
+      });
+  };
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+};
+
+export default App;
