d9afad1c669c5ca36062bdc7be2bbd1d85d563fa
hbjs97 <hbjs97@naver.com>
2023-11-27 01:43:52 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 
 const App = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [authStatus, setAuthStatus] = useState('pending'); // 인증 상태: 'pending', 'success', 'error'
 
   const authenticate = async () => {
     const code = searchParams.get('code');
@@ -11,18 +12,23 @@ const App = () => {
       .post('https://4ce2eof4f3.execute-api.ap-northeast-2.amazonaws.com/authenticate', { code })
       .then((response) => {
         console.log('Server response:', response.data);
+        setAuthStatus('success'); // 인증 성공 시 상태를 'success'로 설정
       })
       .catch((error) => {
         console.error('Error sending code to the server:', error);
+        setAuthStatus('error'); // 에러 발생 시 상태를 'error'로 설정
       });
   };
+
   useEffect(() => {
     authenticate();
   }, []);
 
   return (
     <div>
-      <h1>React App</h1>
+      {authStatus === 'pending' && <p>인증 중...</p>}
+      {authStatus === 'success' && <p>인증 성공!</p>}
+      {authStatus === 'error' && <p>인증 실패. 다시 시도해주세요.</p>}
     </div>
   );
 };
