599ecbe4a8895c5fad8d5b70edc781fd1299fcaa
hbjs97 <hbjs97@naver.com>
2024-01-03 21:07:07 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useSearchParams } from 'react-router-dom'
 
 const App = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [authStatus, setAuthStatus] = useState('pending'); // 인증 상태: 'pending', 'success', 'error'
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [authStatus, setAuthStatus] = useState('pending') // 인증 상태: 'pending', 'success', 'error'
 
   const [response, setResponse] = useState({
     token: {
@@ -17,26 +17,26 @@ const App = () => {
       displayName: '',
       role: '',
     },
-  });
+  })
 
   const authenticate = async () => {
-    const code = searchParams.get('code');
+    const code = searchParams.get('code')
     axios
-      .post('https://4ce2eof4f3.execute-api.ap-northeast-2.amazonaws.com/api/v1/auth', { code })
+      .post('https://external-api.daepyo.org/api/v1/auth', { code, platform: 'WEB' })
       .then((response) => {
-        console.log('Server response:', response.data);
-        setResponse(response.data);
-        setAuthStatus('success'); // 인증 성공 시 상태를 'success'로 설정
+        console.log('Server response:', response.data)
+        setResponse(response.data)
+        setAuthStatus('success') // 인증 성공 시 상태를 'success'로 설정
       })
       .catch((error) => {
-        console.error('Error sending code to the server:', error);
-        setAuthStatus('error'); // 에러 발생 시 상태를 'error'로 설정
-      });
-  };
+        console.error('Error sending code to the server:', error)
+        setAuthStatus('error') // 에러 발생 시 상태를 'error'로 설정
+      })
+  }
 
   useEffect(() => {
-    authenticate();
-  }, []);
+    authenticate()
+  }, [])
 
   return (
     <div>
@@ -54,7 +54,7 @@ const App = () => {
       )}
       {authStatus === 'error' && <p>인증 실패. 다시 시도해주세요.</p>}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
